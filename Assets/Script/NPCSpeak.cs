d15631244a93a3b2bc8a0f574a36c4dcbc7ff5bb
KieranClem <76912798+KieranClem@users.noreply.github.com>
2022-03-15 11:34:03 +0000

    Added text from NPCs, Water collection changes, preparing for future dev
    Added text that can be displayed above NPC with text the dev can add in. Made changes to water to actually be collected and useable rather then just adding to player hydration. Made preparations for adding saveable NPCs

---
+++ Assets/Script/NPCSpeak.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NPCSpeak : MonoBehaviour
+{
+    public string[] spokenDio;
+    public bool savedNPC;
+
+    public Text diolagueBox;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            if (!savedNPC)
+            {
+                StartCoroutine(DisplayText(spokenDio[0]));
+            }
+            else
+            {
+                StartCoroutine(DisplayText(spokenDio[1]));
+            }
+        }
+    }
+
+    public IEnumerator DisplayText(string textToSay)
+    {
+        Vector3 screenPos = Camera.main.WorldToScreenPoint(this.transform.position);
+        screenPos.y += 30;
+        diolagueBox.gameObject.SetActive(true);
+        diolagueBox.text = textToSay;
+        diolagueBox.transform.position = screenPos;
+
+        yield return new WaitForSeconds(6f);
+
+        diolagueBox.gameObject.SetActive(false);
+
+    }
+}
--- Assets/Script/PlayerMovement.cs
+++ Assets/Script/PlayerMovement.cs
@@ -10,17 +10,24 @@ public class PlayerMovement : MonoBehaviour
     public Rigidbody rb;
     public float MovementSpeed = 10f;
 
+    //other gameplay elements
+    private bool nextToSaveNPC;
+    private GameObject NPCToSave;
+
     //UI Elements
     public Text hydrationLevel;
+    public Text numOfWaterBottlesText;
+    public Button waterUseButton;
 
     //Survival mechanics
     [HideInInspector] public int LevelOfWater = 5;
     public int numIncreaseForColWater;
+    public int numOfWater;
 
     // Start is called before the first frame update
     void Start()
     {
-        UpdateWater();
+        hydrationLevel.text = "Hydration: " + LevelOfWater.ToString();
     }
 
     // Update is called once per frame
@@ -40,14 +47,47 @@ public class PlayerMovement : MonoBehaviour
     {
         if(other.tag == "BottleOfWater")
         {
-            LevelOfWater += numIncreaseForColWater;
+            numOfWater += 1;
             UpdateWater();
             Destroy(other.gameObject);
+        }
+
+        if(other.tag == "NPCToSave")
+        {
+            nextToSaveNPC = true;
+            NPCToSave = other.gameObject;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "NPCToSave")
+        {
+            nextToSaveNPC = false;
+            NPCToSave = null;
         }
     }
 
     public void UpdateWater()
     {
+        numOfWaterBottlesText.text = numOfWater.ToString();
+    }
+
+    public void UpdateHydration()
+    {
+        
         hydrationLevel.text = "Hydration: " + LevelOfWater.ToString();
+    }
+
+    public void UseWater()
+    {
+        numOfWater -= 1;
+        if (!nextToSaveNPC)
+        {
+            LevelOfWater += numIncreaseForColWater;
+            UpdateHydration();
+        }
+
+        UpdateWater();
     }
 }
