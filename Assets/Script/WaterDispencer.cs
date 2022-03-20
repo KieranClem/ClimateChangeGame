f9ead8da7a4655103ceaa56bc9737094aaa9e959
KieranClem <76912798+KieranClem@users.noreply.github.com>
2022-03-20 15:38:35 +0000

    Added Water dispenser
    Added water dispenser to allow the player to refill water to prevent the player from being softlocked.

---
+++ Assets/Script/WaterDispencer.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterDispencer : MonoBehaviour
+{
+    public int numberOfWaterToBeGiven;
+    private bool nextToPlayer;
+    private PlayerMovement player;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(nextToPlayer)
+        {
+            if(Input.GetKeyDown(KeyCode.E))
+            {
+                player.numOfWater = numberOfWaterToBeGiven;
+                player.UpdateWater();
+            }
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            nextToPlayer = true;
+            player = other.GetComponent<PlayerMovement>();
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            nextToPlayer = false;
+            player = null;
+        }
+    }
+}
